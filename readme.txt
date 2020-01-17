84742ceb1c8b21dab8dfcd7dadc47992f0667844
DaLei Sun <sundalei1988@gmail.com>
2020-01-17 16:25:54 +0800

    test

---
+++ readme.txt
@@ -0,0 +1,5 @@
+mvn clean install
+
+cd target
+
+java -cp monitor-resources-1.0-SNAPSHOT.jar monitor.Main
--- src/main/java/monitor/Main.java
+++ src/main/java/monitor/Main.java
@@ -1,8 +1,21 @@
 package monitor;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class Main {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        Class<?> anchor = Class.forName("monitor.resources.opened.Anchor");
+
+        URL pack = anchor.getResource("file.txt");
+        URL root = anchor.getResource("/file.txt");
+        URL meta = anchor.getResource("/META-INF/file.txt");
+        URL bytecode = anchor.getResource("Anchor.class");
+
+        System.out.println(pack);
+        System.out.println(root);
+        System.out.println(meta);
+        System.out.println(bytecode);
     }
 }
